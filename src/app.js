59789d8c59aea35da6dfdd9f4c90e4a3c881d97e
Swati Bannad <swatibannad@gmail.com>
2024-09-23 12:22:53 +0530

    dynamc API practice:get,post,delete,patch

--- src/app.js
+++ src/app.js
@@ -4,6 +4,75 @@ const { User } = require("./model/user");
 const app = express();
 
 app.use(express.json());
+
+//getting user by email
+// app.get("/feed" , async (req,res) => {
+
+//     const emailId = req.body.email;
+//     const users = await User.findOne({email:emailId});
+//     try {
+//         res.send(users);
+//     }
+//     catch {
+//         res.status(400).send("problem in signing in!!!")
+//     }
+// });
+//get all the users on user collection
+
+app.get("/feed" , async (req,res) => {
+
+    const users = await User.find();
+    try {
+        res.send(users);
+    }
+    catch {
+        res.status(400).send("problem in signing in!!!")
+    }
+});
+
+//creating delete API
+app.delete("/user", async (req,res) => {
+    const userId = req.body._id;
+    const deleteuser = await User.findByIdAndDelete(userId);
+
+    try {
+        res.send(deleteuser);
+    }
+    catch {
+        res.status(400).send("problem in deleting!!!")
+    }
+});
+
+//creating update API with  ID
+app.patch("/user" , async (req,res) => {
+    const userId = req.body._id;
+    const emailId = req.body.email;
+    const updateduser = await User.findByIdAndUpdate(userId, {email: emailId});
+
+    try {
+        res.send(updateduser);
+    }
+    catch {
+        res.status(400).send("problem in getting matching user")
+    }
+});
+
+//creating update API with first name
+// app.patch("/user" , async (req,res) => {
+//     const userId = req.body._id;
+//     const emailId = req.body.email;
+//     const userName = req.body.firstName;
+//     const query = { firstName: userName };
+//     const updateduser = await User.findOneAndUpdate(query, {email: emailId});
+
+//     try {
+//         res.send(updateduser);
+//     }
+//     catch {
+//         res.status(400).send("problem in getting matching user")
+//     }
+// });
+
 app.post("/signup", async (req, res) => {
     // console.log(req.body);
     //creating a new instance in User model()
@@ -14,7 +83,7 @@ app.post("/signup", async (req, res) => {
         res.send("first user signed up")
     }
     catch {
-        res.send("problem in signing in!!!")
+        res.status(400).send("problem in signing in!!!")
     }
 })
 
--- src/model/user.js
+++ src/model/user.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     firstName : String,
-    lastName: String,
     email: String,
     age: Number,
     gender: String
